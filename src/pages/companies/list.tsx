ef5471444a4d437a36aeec8774bcc36248c66ee3
Anitha <anitha.r@krypc.com>
2023-12-20 14:54:07 +0530

    column added for companies

--- src/pages/companies/list.tsx
+++ src/pages/companies/list.tsx
@@ -15,6 +15,48 @@ export const CompaniesList: React.FC<IResourceComponentsProps> = () => {
     const columns = React.useMemo<GridColDef[]>(
         () => [
             {
+                field: "account_name",
+                headerName: "account_name",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
+                field: "sector",
+                headerName: "sector",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
+                field: "industry_vertical",
+                headerName: "industry_vertical",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
+                field: "area",
+                headerName: "Area",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
+                field: "subsidiary",
+                headerName: "subsidiary",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
+                field: "sales_unit",
+                headerName: "sales_unit",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+            },
+            {
                 field: "actions",
                 headerName: "Actions",
                 sortable: false,
@@ -29,7 +71,7 @@ export const CompaniesList: React.FC<IResourceComponentsProps> = () => {
                 },
                 align: "center",
                 headerAlign: "center",
-                minWidth: 80,
+                //minWidth: 80,
             },
         ],
         [],
