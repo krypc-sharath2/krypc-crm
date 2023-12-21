a21bf919285326c690013699aa4e3b978dfa6fbe
Anitha <anitha.r@krypc.com>
2023-12-21 13:22:48 +0530

    column added for pillars , marketResearch

--- src/pages/pillars/list.tsx
+++ src/pages/pillars/list.tsx
@@ -22,18 +22,68 @@ export const PillarList: React.FC<IResourceComponentsProps> = () => {
                 headerAlign: "center",
             },
             {
-                field: "industry",
-                headerName: "industry",
+                field: "dApps",
+                headerName: "dApps",
                 sortable: false,
                 align: "center",
                 headerAlign: "center",
+                headerClassName :'child-header'
             },
             {
-                field: "industry_vertical",
-                headerName: "industry_vertical",
+                field: "SmartContract Automation",
+                headerName: "SmartContract Automation",
                 sortable: false,
                 align: "center",
                 headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "NFTs",
+                headerName: "NFTs",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "Permissioned Blockchain",
+                headerName: "Permissioned Blockchain",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "Supplychain",
+                headerName: "Supplychain",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "Self Sovereign Identity",
+                headerName: "Self Sovereign Identity",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "DeFi Solutions",
+                headerName: "DeFi Solutions",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
+            },
+            {
+                field: "Provenance",
+                headerName: "Provenance",
+                sortable: false,
+                align: "center",
+                headerAlign: "center",
+                headerClassName :'child-header'
             },
             {
                 field: "actions",
@@ -57,8 +107,34 @@ export const PillarList: React.FC<IResourceComponentsProps> = () => {
     );
 
     return (
-        <List>
-            <DataGrid {...dataGridProps} columns={columns} autoHeight />
+        <List contentProps={{sx: {
+                '& .my-group-header': {
+                    backgroundColor: 'blue',
+                },
+                '& .hlf-group-header': {
+                    backgroundColor: 'blueviolet',
+                },
+                '& .child-header': {
+                    backgroundColor: 'yellow',
+                },
+            }}}>
+            <DataGrid   experimentalFeatures={{ columnGrouping: true }}
+                        {...dataGridProps} columns={columns} columnGroupingModel={[
+                {
+                    groupId: 'Web3',
+                    headerAlign: "center",
+                    headerClassName: 'my-group-header',
+                    children: [{ field: 'dApps' },{ field: 'SmartContract Automation' },{ field: 'NFTs' }],
+                },
+                {
+                    groupId: 'HLF',
+                    headerAlign: "center",
+                    headerClassName: 'hlf-group-header',
+                    children: [{ field: 'Permissioned Blockchain' },{ field: 'Supplychain' },{ field: 'Self Sovereign Identity' },
+                        { field: 'DeFi Solutions' },{ field: 'Provenance' }],
+                },
+
+            ]} autoHeight />
         </List>
     );
 };
